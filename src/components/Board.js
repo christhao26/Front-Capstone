3775707d94b6b3c7dc00ded29b71446980a77803
christhao26 <79154880+christhao26@users.noreply.github.com>
2021-07-19 13:06:42 -0400

    updated front end

--- src/components/Board.js
+++ src/components/Board.js
@@ -7,87 +7,90 @@ export default class Board extends Component {
   constructor(props) {
     super(props);
     //if there's a localStorage to be had grab it otherwise set state
-    if(localStorage.getItem('lists')) {
+    // if(localStorage.getItem('lists')) {
       const rawLS = localStorage.getItem('lists');
       const parsedLS = JSON.parse(rawLS);
-      this.state = { lists: parsedLS }
-    } else {
-      this.state = {
-        lists: [
-          {
-            title: 'Back-log!',
-            id: 0,
-            cards: [{
-              taskText: 'Tasks',
-              listNumber: 0,
-              timeId: 0
-            }, 
-            {
-              taskText: 'Task 2',
-              listNumber: 0,
-              timeId: 1
-            }]
-          },
-          {
-            title: 'ToDo',
-            id: 1,
-            cards: [{
-              taskText: 'Task 1',
-              listNumber: 1,
-              timeId: 2
-            }, 
-            {
-              taskText: 'Task 2',
-              listNumber: 1,
-              timeId: 3
-            }]
-          },
-          {
-            title: 'In Progress',
-            id: 2,
-            cards: [{
-              taskText: 'Task 1',
-              listNumber: 2,
-              timeId: 4
-            }, 
-            {
-              taskText: 'Task 2',
-              listNumber: 2,
-              timeId: 5
-            }]
-          },
-          {
-            title: 'Done!',
-            id: 3,
-            cards: [{
-              taskText: 'Task 1',
-              listNumber: 3,
-              timeId: 6
-            }, 
-            {
-              taskText: 'Task 2',
-              listNumber: 3,
-              timeId: 7
-            }]
-          }
-        ]
-      }
+      this.state = { lists: [] }
+    // } else {
+      // this.state = {
+        // lists: [
+        //   {
+        //     title: 'Back-log1122!',
+        //     id: 0,
+        //     cards: [{
+        //       taskText: 'Tasks',
+        //       listNumber: 0,
+        //       timeId: 0
+        //     }, 
+        //     {
+        //       taskText: 'Task 2',
+        //       listNumber: 0,
+        //       timeId: 1
+        //     }]
+        //   },
+        //   {
+        //     title: 'ToDo',
+        //     id: 1,
+        //     cards: [{
+        //       taskText: 'Task 1',
+        //       listNumber: 1,
+        //       timeId: 2
+        //     }, 
+        //     {
+        //       taskText: 'Task 2',
+        //       listNumber: 1,
+        //       timeId: 3
+        //     }]
+        //   },
+        //   {
+        //     title: 'In Progress',
+        //     id: 2,
+        //     cards: [{
+        //       taskText: 'Task 1',
+        //       listNumber: 2,
+        //       timeId: 4
+        //     }, 
+        //     {
+        //       taskText: 'Task 2',
+        //       listNumber: 2,
+        //       timeId: 5
+        //     }]
+        //   },
+        //   {
+        //     title: 'Done!',
+        //     id: 3,
+        //     cards: [{
+        //       taskText: 'Task 1',
+        //       listNumber: 3,
+        //       timeId: 6
+        //     }, 
+        //     {
+        //       taskText: 'Task 2',
+        //       listNumber: 3,
+        //       timeId: 7
+        //     }]
+        //   }
+        // ]
+      // }
 
       localStorage.setItem('lists', JSON.stringify(this.state.lists))
-    }
+    // }
   }
 componentDidMount = () => {
  axios({
     method: 'get',
-    url: "http://localhost:8080/api/todoItems",
+    url: "http://localhost:8080/api/lists",
     headers: {
      credentials: 'include',
     }
   })
   .then(data => {
-    console.log(data, "data from board")
+    console.log(data.data, "data from board")
+    this.setState({lists: data.data})
+
   })
 }
+
   //get id of item being dragged and list where it's coming from
   onDragStart = (e, fromList) => {
     console.log(`what a drag!`)
@@ -149,10 +152,11 @@ componentDidMount = () => {
 
 
 render() {
+  console.log(this.state)
   const lists = this.state.lists.map((list, index) => (
     <li className="list-wrapper" key={index}>
       <List {...list} 
-        onAdd={(taskText, listNumber) => this.addTaskCard(taskText, listNumber)} 
+        // onAdd={(taskText, listNumber) => this.addTaskCard(taskText, listNumber)} 
         onDragStart={(e, fromList) => this.onDragStart(e, `${list.id}`)}
         onDragOver={(e) => this.onDragOver(e)} 
         onDrop={(e, listNum) => {this.onDrop(e, `${list.id}`)}}
