9d29d2bbcdd6dd26943499cbc6b6678521752162
SophieCam <79154836+SophieCam@users.noreply.github.com>
2021-07-20 11:55:02 -0400

    updated front-end

--- src/components/Board.js
+++ src/components/Board.js
@@ -11,6 +11,7 @@ export default class Board extends Component {
       // const rawLS = localStorage.getItem('lists');
       // const parsedLS = JSON.parse(rawLS);
       this.state = { lists: [] }
+      this.state.draggedInfo= {}
     // } else {
       // this.state = {
         // lists: [
@@ -98,22 +99,25 @@ localStorage.setItem("lists",JSON.stringify(this.state.lists))
     console.log(fromList)
     const dragInfo = {
       taskId: e.currentTarget.id,
+      taskText: e.currentTarget.innerHTML,
       fromList: fromList
     }
   
     localStorage.setItem('dragInfo', JSON.stringify(dragInfo));
+    this.setState({draggedInfo: dragInfo})
+    console.log(this.state)
   }
-componentDidUpdate(){
-  console.log(this.state.lists)
-  axios({
-    method: "post",
-    url:"http://localhost:8080/api/lists/post/all",
-    headers: {
-      credentials: 'include',
-    },
-    data:this.state.lists
-  })
-  }
+// componentDidUpdate(){
+//   console.log(this.state.lists)
+//   axios({
+//     method: "post",
+//     url:"http://localhost:8080/api/lists/post/all",
+//     headers: {
+//       credentials: 'include',
+//     },
+//     data:this.state.lists
+//   })
+//   }
 
   onDragOver = (e) => {
     e.preventDefault();
@@ -123,18 +127,21 @@ componentDidUpdate(){
     //get the dropped task card, the localStorage, 
     const droppedTask = localStorage.getItem('dragInfo');
     const rawLS = localStorage.getItem('lists');
-   
     const parsedLS = JSON.parse(rawLS);
-    console.log(parsedLS)
+    console.log(rawLS)
     const parsedDragInfo = JSON.parse(droppedTask)
-    
+
     //get task cards array, get rid of moved card, and put a new card
     // in the list where it was dropped
     console.log(parsedDragInfo)
-    console.log(parsedLS[parsedDragInfo.fromList ])
+    console.log(rawLS[parsedDragInfo.fromList ])
+    const fromLists = parsedDragInfo.fromList
+    console.log(Object.values(rawLS))
     const cardsArray = parsedLS[parsedDragInfo.fromList].cards
-    const taskCard = cardsArray.find(card => card.timeId === parsedDragInfo.taskId)
-    const indexOfCard = cardsArray.findIndex(card => card.timeId === parsedDragInfo.taskId)
+    console.log(cardsArray)
+    const taskCard = cardsArray.find(card => card.timeId == parsedDragInfo.taskId)
+    console.log(taskCard)
+    const indexOfCard = cardsArray.findIndex(card => card.timeId == parsedDragInfo.taskId)
     parsedLS[parsedDragInfo.fromList].cards.splice(indexOfCard, 1)
     parsedLS[listNum].cards.push({...taskCard, listNumber: parseInt(listNum)})
    
@@ -183,6 +190,7 @@ render() {
    
   return (
     <div className="board">
+      <h1>hiiiii</h1>
       <ul className="lists">
         {lists}
       </ul>
